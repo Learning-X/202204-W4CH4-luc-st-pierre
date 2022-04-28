b98bc54e7731cd676fd9294159868c787d61af09
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-28 23:02:51 +0200

    Created Arrange Act and Assert

--- src/components/Info/Info.test.js
+++ src/components/Info/Info.test.js
@@ -1,5 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import PhoneContextProvider from "../../contexts/PhoneContextProvider";
+import Info from "./Info";
+
 describe("Given an Info Component", () => {
   describe("When it's being rendered", () => {
-    test("Then it should render a span element with text 'Calling...", () => {});
+    test("Then it should render a span element with text 'Calling...", () => {
+      const text = "Calling...";
+
+      render(
+        <PhoneContextProvider>
+          <Info />
+        </PhoneContextProvider>
+      );
+
+      const expectedText = screen.getByText(text);
+      expect(expectedText).toBeInTheDocument();
+    });
   });
 });
