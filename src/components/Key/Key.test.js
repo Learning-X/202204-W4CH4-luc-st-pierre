2d822248b869f5fc9d2a9c63d37f38b991a50ce7
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-28 23:08:09 +0200

    Added describes test

---
+++ src/components/Key/Key.test.js
@@ -0,0 +1,5 @@
+describe("Given a Key Component", () => {
+  describe("When instantiated with a key '7' as text", () => {
+    test("Then it should render a button element with text '7", () => {});
+  });
+});
