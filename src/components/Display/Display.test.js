f4f57c2c672797a7440839cba12b6441a2610a0f
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-29 00:09:08 +0200

    Change numbers and create  Arrange Act and Assert

--- src/components/Display/Display.test.js
+++ src/components/Display/Display.test.js
@@ -1,5 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import PhoneContext from "../../contexts/PhoneContext";
+import PhoneContextProvider from "../../contexts/PhoneContextProvider";
+import Display from "./Display";
+
 describe("Given a Display component", () => {
   describe("When instantiated", () => {
-    test("Then it should render with text '123456789'", () => {});
+    test("Then it should render with text '329019288'", () => {
+      const numbers = "329019288";
+      const expectedNumbers = { phoneNumber: numbers };
+
+      render(
+        <PhoneContext.Provider value={expectedNumbers}>
+          <Display />
+        </PhoneContext.Provider>
+      );
+
+      const displayResult = screen.getByText(numbers);
+      expect(displayResult).toBeInTheDocument();
+    });
   });
 });
