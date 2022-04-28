b9a20fb28dd21a14e389a4ddfd5a0fdc0a87a969
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-28 22:58:06 +0200

    Added describes test

---
+++ src/components/Info/Info.test.js
@@ -0,0 +1,5 @@
+describe("Given an Info Component", () => {
+  describe("When it's being rendered", () => {
+    test("Then it should render a span element with text 'Calling...", () => {});
+  });
+});
