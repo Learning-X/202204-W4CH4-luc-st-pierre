b894af992476d717b0e4e6b0da781050687e006e
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-28 23:11:54 +0200

    Created Arrange Act and Assert

--- src/components/Key/Key.test.js
+++ src/components/Key/Key.test.js
@@ -1,5 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Key from "./key";
+
 describe("Given a Key Component", () => {
   describe("When instantiated with a key '7' as text", () => {
-    test("Then it should render a button element with text '7", () => {});
+    test("Then it should render a button element with text '7", () => {
+      const text = "7";
+
+      render(<Key disabled={true} big={true} text={text} />);
+
+      const expectedText = screen.getByText(text);
+
+      expect(expectedText).toBeInTheDocument();
+    });
   });
 });
