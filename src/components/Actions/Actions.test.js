c27d9255b39caed02e2afbdb87f9382665b426a1
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-29 00:29:49 +0200

    Added describes test and created Arrange Act and Assert

--- src/components/Actions/Actions.test.js
+++ src/components/Actions/Actions.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import PhoneContext from "../../contexts/PhoneContext";
+import PhoneContextProvider from "../../contexts/PhoneContextProvider";
 import Actions from "./Actions";
 
 describe("Given an Action component", () => {
+  describe("when instantiated", () => {
+    test("Then it should render an Action with a class 'call'", () => {
+      render(
+        <PhoneContextProvider>
+          <Actions />
+        </PhoneContextProvider>
+      );
+      const result = screen.getByRole("link", { class: "Call" });
+
+      expect(result).toBeInTheDocument();
+    });
+  });
+
   describe("When instantiated while calling value of true", () => {
     test("Then it should render with display and an action button hang", () => {
       const calling = true;
